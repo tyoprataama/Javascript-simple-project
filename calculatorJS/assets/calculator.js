282a0a333d6ea1b4c743a9327c6e45deba5e22e9
tyoprataama <tyoprataama004@gmail.com>
2022-07-08 22:00:20 +0700

    Calculator

---
+++ calculatorJS/assets/calculator.js
@@ -0,0 +1,32 @@
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false,
+};
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll('.button');
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+        // mendapatkan objek elemen yang diklik
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
