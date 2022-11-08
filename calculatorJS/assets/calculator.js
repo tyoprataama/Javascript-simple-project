72513902c862c70e6bff6f9185a61a5652d1d117
tyoprataama <tyoprataama004@gmail.com>
2022-11-08 09:29:40 +0700

    refactor calculator

--- calculatorJS/assets/calculator.js
+++ calculatorJS/assets/calculator.js
@@ -50,7 +50,15 @@ function performCalculation() {
     } else {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
 const buttons = document.querySelectorAll('.button');
